8a07c329366ece2ca03ec3979ab47ed4226a96bd
karlos1998 <fadeusz98@gmail.com>
2023-02-23 19:13:10 +0100

    sh file fix

--- sim-app.sh
+++ sim-app.sh
@@ -2,19 +2,22 @@
 
 # Definition of variables with image name and Dockerfile path
 IMAGE_NAME="karlos98/sim-app-device"
-DOCKERFILE_PATH="https://github.com/Let-s-Code-It/SIM-App-Device.git"
+DOCKERFILE_URL="https://raw.githubusercontent.com/Let-s-Code-It/SIM-App-Device/master/Dockerfile"
+DOCKERFILE_PATH="Sim-App-Dockerfile"
 
 # Check if "update" argument is passed
 if [ "$1" == "update" ]; then
   echo "Building the image without cache..."
   # Build the image without using cache
-  docker build --no-cache -t "$IMAGE_NAME" -f- "$DOCKERFILE_PATH"
+  curl -o "$DOCKERFILE_PATH" "$DOCKERFILE_URL"
+  docker build --no-cache -t "$IMAGE_NAME" -f "$DOCKERFILE_PATH" .
 else
   # Check if the image exists
   if ! docker image inspect "$IMAGE_NAME" &> /dev/null; then
     echo "Image doesn't exist. Building..."
     # Build the image
-    docker build -t "$IMAGE_NAME" -f- "$DOCKERFILE_PATH"
+    curl -o "$DOCKERFILE_PATH" "$DOCKERFILE_URL"
+    docker build -t "$IMAGE_NAME" -f "$DOCKERFILE_PATH" .
   fi
 fi
 
